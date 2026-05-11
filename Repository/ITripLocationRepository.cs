1ec8a52d8da5ab1d30876aa90d5af97b34baad06
Diellza Behadini <diellzab2003@gmail.com>
2026-05-11 21:40:56 +0200

    Adding more repos

--- Repository/ITripLocationRepository.cs
+++ Repository/ITripLocationRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace SOAP.Repository
+﻿using SOAP.Models;
+
+namespace SOAP.Repository
 {
     public interface ITripLocationRepository
     {
+        Task AddAsync(TripLocation tripLocation);
+        Task<List<TripLocation>> GetByTripIdAsync(Guid tripId);
+        Task SaveAsync();
     }
 }
